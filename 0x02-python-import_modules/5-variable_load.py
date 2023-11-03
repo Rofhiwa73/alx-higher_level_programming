06f1aa6b575f0c631ddf66a51bd442520e17b82c
Henessy <125590551+Rofhiwa73@users.noreply.github.com>
2023-11-02 20:05:56 -0700

    Everything can be imported

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Imports the variable a from file variable_load_5.py and prints value.
+
+if __name__ == "__main__":
+    import variable_load_5
+
+    a = variable_load_5.a
+
+    print("{:d}".format(a))
